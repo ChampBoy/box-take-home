9e2cc3102cfc5593215f9d5d54b9ecd323c98ab7
ChampBoy <parawalasunny@gmail.com>
2019-10-28 22:42:29 -0500

    Initial Setup and test

--- src/Driver.java
+++ src/Driver.java
@@ -3,16 +3,18 @@ import java.util.*;
 
 public class Driver
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
-        Utils u = new Utils();
-        List<Utils.InitialPosition> initialPieces=new ArrayList<>();
-        List<String> upperCaptures=new ArrayList<>();
-        List<String> lowerCaptures=new ArrayList<>();
-        List<String> moves=new ArrayList<>();
-        initialPieces.add(new Utils.InitialPosition("K","e4"));
-        initialPieces.add(new Utils.InitialPosition("s","a2"));
-        Utils.TestCase tc = new Utils.TestCase(initialPieces,upperCaptures,lowerCaptures,moves);
-        System.out.println(tc.toString());
+//        Utils u = new Utils();
+//        List<Utils.InitialPosition> initialPieces=new ArrayList<>();
+//        List<String> upperCaptures=new ArrayList<>();
+//        List<String> lowerCaptures=new ArrayList<>();
+//        List<String> moves=new ArrayList<>();
+//        initialPieces.add(new Utils.InitialPosition("K","e4"));
+//        initialPieces.add(new Utils.InitialPosition("s","a2"));
+//        Utils.TestCase tc = new Utils.TestCase(initialPieces,upperCaptures,lowerCaptures,moves);
+        Utils.TestCase temp = Utils.parseTestCase("/Users/ILYASPARAWALA/Desktop/UMN Stuff/box-take-home/test/BoxShogi Test Cases/basicCheck.in");
+
+        System.out.println(temp.toString());
     }
 }
